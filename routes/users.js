3394b2847031fe1a520a95071398ccdf34c33f0a
Guibert Denis <guibert.denis@yahoo.com>
2025-10-21 18:13:30 +0200

    Correction made on routes/users.js on routes setup

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 /* ADD CRUD */
 router.get("/users/", getAllUsers);
-router.get("/users/:id", getUserById);
-router.post("/users/:email", CreateUser);
+router.get("/users/:email", getUserByEmail);
+router.post("/users/", CreateUser);
 router.put("/users/:email", updateUser);
 router.delete("/users/:email", deleteUser);
 
